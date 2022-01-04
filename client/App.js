293bbe89a6dd5f05fb58a0b5a8644cba6f9fa6c4
tabka.sahar <tabkasahar3@gmail.com>
2022-01-04 20:16:32 +0100

    displaying service providers list

--- client/App.js
+++ client/App.js
@@ -1,12 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ServiceProviderList from './components/ServiceProviderList.js'
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>hello sahra </Text>
-      <StatusBar style="auto" />
-    </View>
+ <ServiceProviderList/>
   );
 }
 
