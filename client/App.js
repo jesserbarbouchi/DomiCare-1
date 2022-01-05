621b06334c2b5015f9de0aa7620d6e39e55dab59
REZGUI-Mohamed <mohamedrezguimedrezz@gmail.com>
2022-01-05 10:51:02 +0100

    med

--- client/App.js
+++ client/App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { SafeAreaView, StyleSheet, TextInput } from "react-native";
 
-export default function App() {
+const UselessTextInput = () => {
+  const [text, onChangeText] = React.useState("Useless Text");
+  const [number, onChangeNumber] = React.useState(null);
+
   return (
-    <View style={styles.container}>
-      <Text>hello sahra </Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="useless placeholder"
+        keyboardType="numeric"
+      />
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'red',
-    alignItems: 'center',
-    justifyContent: 'center',
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
+
+export default UselessTextInput;
