adba5998dd8bfb196b61188f6c131873a6af2a8d
jesserbarbouchi <jesserbarbouchi@gmail.com>
2022-01-05 10:43:09 +0100

    Displaying the items in the equipements feed

--- client/router.js
+++ client/router.js
@@ -7,6 +7,7 @@ import Home from './components/Home.js'
 const Stack = createNativeStackNavigator()
 const Router = () => {
   return (
+    //create your routes here
     <Stack.Navigator>
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="EquipementsFeed" component={EquipementsFeed} />
