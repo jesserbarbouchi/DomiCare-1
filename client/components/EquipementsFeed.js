2754e0f37826e5d22a488d7a88c44988893191d2
jesserbarbouchi <jesserbarbouchi@gmail.com>
2022-01-07 20:07:26 +0100

    merge

--- client/components/EquipementsFeed.js
+++ client/components/EquipementsFeed.js
@@ -1,5 +1,5 @@
 import React,{useState, useEffect} from 'react'
-import axios from 'axios'
+import { Picker } from '@react-native-picker/picker';
 import { View, Text,StyleSheet,SafeAreaView,ScrollView,Image, Button } from 'react-native'
 import items from "./Equipements.js"
 
