1ff4e5ec2ae4e1e9af99e844617355cabafbc05b
tabka.sahar <tabkasahar3@gmail.com>
2022-01-17 22:30:37 +0100

    get all and delete equipements questions/answers - posts- reports

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -1,6 +1,9 @@
 const adminLogin = require("../models/Admins.js");
 const bcrypt = require("bcrypt");
 const Posts = require("../models/Posts.js")
+const report = require('../models/reports.js')
+const Equipement = require("../models/Equipements.js")
+const QuestAns = require("../models/Question&Answers");
 
 
 
@@ -21,27 +24,82 @@ module.exports = {
         
     }
     catch (err) {
-        res.send(err);
+        res.send(err)
     }
         
     },
     delete_post: async (req, res) => {
-        const  postId  = req.params._id
+        const postId = req.params.id
         console.log(postId)
 
 
         try {
-            const data = await Posts.deleteOne({_id:postId}).
-            res.send(data)
-            
+            const data = await Posts.findByIdAndRemove({_id:postId})   
         }
         catch (error) { console.log('err', error) }
+          
+    },
+    get_reports: async (req, res) => {
+        try {
+            const reports = await report.find({})
+             res.send(reports)
+         
+     }
+     catch (err) {
+         res.send(err);
+     }
+         
+    },
+    get_equipement: async (req, res) => {
+        try {
+            const equip = await Equipement.find({})
+            res.send(equip)
+         
+        }
+        catch (err) {
+            res.send(err);
+        }
+    },
+    delete_equipement: async (req, res) => {
+        const equipId = req.params.id
+        console.log(postId)
+
+
+        try {
+            const data = await Equipement.findByIdAndRemove({_id:equipId})   
+        }
+        catch (error) { console.log('err', error) }
+          
+    },
+
+
+
+
+
+
+    get_quesans: async (req, res) => {
+        try {
+            const quesan = await QuestAns.find({})
+            res.send(quesan)
+             
+        }
+        catch (err) {
+            res.send(err);
+        }
+    },
+    delete_quesans: async (req, res) => {
+        const quesans = req.params.id
+        console.log(quesans)
+
+            
+        try {
+            const data = await QuestAns.findByIdAndRemove({_id:quesans})   
+        }
+        catch (error) { console.log('err', error) }
+          
+    },
         
-
-
-
-        
-    }
+    
 
 
 
